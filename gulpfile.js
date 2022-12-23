360a329893adeb536fb44e90604b2db6585e234f
MaksymDramaretskyi <maksymdramaretskyi@gmail.com>
2022-12-23 22:35:24 +0200

    базовая структура

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+gulp.task ('css', ()=> {
+  return gulp.src('./scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./css'))
+})
